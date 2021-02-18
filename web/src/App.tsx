3febc589ad071c96bcf00c6f90d3ada9ef47724f
anselbrandt <ansel@anselbrandt.com>
2021-02-18 17:42:35 -0500

    Add 'Server time: '

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div>Hello from React.tsx</div>
       <div>{data ? data.message : null}</div>
-      <div>{data ? data.time : null}</div>
+      <div>{data ? `Server time: ${data.time}` : null}</div>
       <div>{error ? "API server error" : null}</div>
     </>
   );
